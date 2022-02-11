d244d4c57a452ff5a3a1bb86357991dfdf3cee3f
Brett Smith <brettsmith212@gmail.com>
2022-02-11 08:16:55 -1000

    get actions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,17 @@
 // add middlewares here related to actions
+const Actions = require("./actions-model");
+
+async function validateAction(req, res, next) {
+  let { id } = req.params;
+  let action = await Actions.get(id);
+  if (!action) {
+    res.status(404).json({ message: "Invalid action ID" });
+    return;
+  }
+  req.action = action;
+  next();
+}
+
+module.exports = {
+  validateAction,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,25 @@
 // Write your "actions" router here!
 const express = require("express");
 const Actions = require("./actions-model");
+const { validateAction } = require("./actions-middlware");
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    let actions = await Actions.get();
+    res.status(200).json(actions);
+  } catch (e) {
+    res.status(500).json({ message: "error getting actions" });
+  }
+});
+
+router.get("/:id", validateAction, async (req, res) => {
+  try {
+    res.status(200).json(req.action);
+  } catch (e) {
+    res.status(500).json({ message: "error getting action" });
+  }
+});
 
 module.exports = router;
