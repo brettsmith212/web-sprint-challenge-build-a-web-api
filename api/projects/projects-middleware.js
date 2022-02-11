0c1aa9ccd4b1d95539c72a11773e8a6aa8ee3de8
Brett Smith <brettsmith212@gmail.com>
2022-02-11 07:32:03 -1000

    router setup | get projects done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,17 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model");
+
+async function validateProject(req, res, next) {
+  let { id } = req.params;
+  let project = await Projects.get(id);
+  if (!project) {
+    res.status(404).json({ message: "Invalid project ID" });
+    return;
+  }
+  req.project = project;
+  next();
+}
+
+module.exports = {
+  validateProject,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,25 @@
 // Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+const { validateProject } = require("./projects-middleware");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    let projects = await Projects.get();
+    res.status(200).json(projects);
+  } catch (e) {
+    res.status(500).json({ message: "error getting projects" });
+  }
+});
+
+router.get("/:id", validateProject, async (req, res) => {
+  try {
+    res.status(200).json(req.project);
+  } catch (e) {
+    res.status(500).json({ message: "error getting project" });
+  }
+});
+
+module.exports = router;
